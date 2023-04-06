07a286ada38fe63bccc46ab72a25bba86756d691
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-04-06 09:46:54 +0700

    deploy to render

--- server/index.js
+++ server/index.js
@@ -12,14 +12,14 @@ const io = new socket_io.Server(httpServer, {
   cors: {
     
     // origin: "http://localhost:3000",
-    origin: "*",
+    origin: "http://localhost:3000",
     methods:['GET',"POST"]
   },
 });
 
 app.use(cors({
   // origin: "http://localhost:3000", // allow to server to accept request from different origin
-  origin: "*",
+  origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // allow session cookie from browser to pass through
 }))
