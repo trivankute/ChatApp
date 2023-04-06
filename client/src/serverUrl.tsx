24efb0e09d3672c516aba3e4a4b7fefd18d4d55a
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-04-06 09:39:35 +0700

    for deploy to render.com

--- client/src/serverUrl.tsx
+++ client/src/serverUrl.tsx
@@ -1,3 +1,3 @@
-const url = "https://chatapp-trivandeptraivodichvutru.onrender.com"
+const url = "https://chatapp-trivandeptraivodichvutru.onrender.com/"
 // const url = "http://localhost:3001/"
 export default url
