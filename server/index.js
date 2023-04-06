a3b5dd288ce6a61e36d8d10614c56321abbf158c
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-04-06 10:04:07 +0700

    change cors origin

--- server/index.js
+++ server/index.js
@@ -12,14 +12,14 @@ const io = new socket_io.Server(httpServer, {
   cors: {
     
     // origin: "http://localhost:3000",
-    origin: "http://localhost:3000",
+    origin: "https://mellifluous-licorice-853b35.netlify.app",
     methods:['GET',"POST"]
   },
 });
 
 app.use(cors({
   // origin: "http://localhost:3000", // allow to server to accept request from different origin
-  origin: "http://localhost:3000",
+  origin: "https://mellifluous-licorice-853b35.netlify.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // allow session cookie from browser to pass through
 }))
